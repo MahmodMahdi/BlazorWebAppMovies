a10d4b387c40e8ebd21611c7eecc7aff1205f3db
MahmoudAmin <mahmoudElmahdy555@gmail.com>
2026-01-27 21:27:55 +0200

    feat: Implement DBContext Class

---
+++ Data/BlazorWebAppMoviesContext.cs
@@ -0,0 +1,21 @@
+﻿using BlazorWebAppMovies.Models;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace BlazorWebAppMovies.Data
+{
+	public class BlazorWebAppMoviesContext : IdentityDbContext<ApplicationUser>
+	{
+		public BlazorWebAppMoviesContext(DbContextOptions<BlazorWebAppMoviesContext> options) : base(options){}
+		public BlazorWebAppMoviesContext(){}
+		public DbSet<Movie> Movie { get; set; }
+		public DbSet<Genre> Genre { get; set; }
+
+		protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+		{
+			base.OnConfiguring(optionsBuilder);
+		}
+
+
+	}
+}
