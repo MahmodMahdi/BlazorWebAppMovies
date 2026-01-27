23953bd283af43623d97b1c69b1802560842d906
MahmoudAmin <mahmoudElmahdy555@gmail.com>
2026-01-27 22:12:06 +0200

    feat: Implement UnitOfWork Pattern for enhancement

---
+++ UnitOfWork/IUnitOfWork.cs
@@ -0,0 +1,16 @@
+﻿using BlazorWebAppMovies.GenericRepo;
+using BlazorWebAppMovies.Models;
+
+namespace BlazorWebAppMovies.UnitOfWork
+{
+	public interface IUnitOfWork : IDisposable
+	{
+		IGenericRepository<Movie> MovieRepository { get; }
+		IGenericRepository<Genre> GenreRepository { get; }
+		Task<int> CompleteAsync();
+		Task BeginTransactionAsync();
+		Task CommitTransactionAsync();
+		Task RollbackTransactionAsync();
+
+	}
+}
---
+++ UnitOfWork/UnitOfWork.cs
@@ -0,0 +1,45 @@
+﻿using BlazorWebAppMovies.Data;
+using BlazorWebAppMovies.GenericRepo;
+using BlazorWebAppMovies.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Storage;
+
+namespace BlazorWebAppMovies.UnitOfWork
+{
+	public class UnitOfWork : IUnitOfWork
+	{
+		private readonly BlazorWebAppMoviesContext _dbcontext;
+		private  IDbContextTransaction _transaction;
+		public IGenericRepository<Movie> MovieRepository { get; }
+		public IGenericRepository<Genre> GenreRepository { get; }
+
+		public UnitOfWork(IDbContextFactory<BlazorWebAppMoviesContext> dbFactory)
+		{
+			_dbcontext = dbFactory.CreateDbContext();
+            MovieRepository = new GenericRepository<Movie>(_dbcontext);
+			GenreRepository = new GenericRepository<Genre>(_dbcontext);
+		}
+
+
+		public async Task<int> CompleteAsync() =>
+			 await _dbcontext.SaveChangesAsync();
+
+		public async Task BeginTransactionAsync() =>
+			_transaction = await _dbcontext.Database.BeginTransactionAsync();
+		public async Task CommitTransactionAsync()
+		{
+			if(_transaction  != null)
+			await _transaction.CommitAsync();
+		}
+		public async Task RollbackTransactionAsync()
+		{
+			if (_transaction != null)
+				await _dbcontext.Database.RollbackTransactionAsync();
+		}
+		public void Dispose()
+		{
+			_transaction?.Dispose();
+			_dbcontext?.Dispose();
+		}
+	}
+}
