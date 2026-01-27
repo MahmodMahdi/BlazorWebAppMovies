08fed70c7c7d59b00a985122aef5b6d913dfa622
MahmoudAmin <mahmoudElmahdy555@gmail.com>
2026-01-27 21:29:16 +0200

    feat: Add Migrations

---
+++ Migrations/20260125151911_AddGenreTable.cs
@@ -0,0 +1,96 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace BlazorWebAppMovies.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddGenreTable : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Genre",
+                table: "Movie");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Title",
+                table: "Movie",
+                type: "nvarchar(60)",
+                maxLength: 60,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)");
+
+            migrationBuilder.AddColumn<int>(
+                name: "GenreId",
+                table: "Movie",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.CreateTable(
+                name: "Genre",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Description = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Genre", x => x.Id);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Movie_GenreId",
+                table: "Movie",
+                column: "GenreId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Movie_Genre_GenreId",
+                table: "Movie",
+                column: "GenreId",
+                principalTable: "Genre",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Movie_Genre_GenreId",
+                table: "Movie");
+
+            migrationBuilder.DropTable(
+                name: "Genre");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Movie_GenreId",
+                table: "Movie");
+
+            migrationBuilder.DropColumn(
+                name: "GenreId",
+                table: "Movie");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Title",
+                table: "Movie",
+                type: "nvarchar(max)",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(60)",
+                oldMaxLength: 60);
+
+            migrationBuilder.AddColumn<string>(
+                name: "Genre",
+                table: "Movie",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+        }
+    }
+}
