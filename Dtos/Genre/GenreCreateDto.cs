98f2acd00df0c4351f76837f9bc33362472d45c8
MahmoudAmin <mahmoudElmahdy555@gmail.com>
2026-01-27 22:08:57 +0200

    feat: Implement Genre Dtos

---
+++ Dtos/Genre/GenreCreateDto.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace BlazorWebAppMovies.Dtos.Genre
+{
+	public class GenreCreateDto
+	{
+		[Required]
+		[StringLength(30, MinimumLength = 3)]
+		public string Name { get; set; }
+		[StringLength(100, MinimumLength = 3)]
+		public string Description { get; set; }
+	}
+}
---
+++ Dtos/Genre/GenreUpdateDto.cs
@@ -0,0 +1,10 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace BlazorWebAppMovies.Dtos.Genre
+{
+	public class GenreUpdateDto : GenreCreateDto
+	{
+		[Required]
+		public int Id { get; set; }
+	}
+}
