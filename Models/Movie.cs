5bb5e9732a22a0aae7bfbac32be68a593bf5edc5
MahmoudAmin <mahmoudElmahdy555@gmail.com>
2026-01-27 21:28:43 +0200

    feat: Implement Models of Application

---
+++ Models/Movie.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace BlazorWebAppMovies.Models
+{
+	public class Movie
+	{
+		public int Id { get; set; }
+		public string Title { get; set; }
+		public DateOnly ReleaseDate { get; set; }	
+		public decimal Price { get; set; }
+		[DisplayName("Genre")]
+		public int GenreId { get; set; }
+		[ForeignKey(nameof(GenreId))]
+		public Genre Genre { get; set; } = default!;
+	}
+}
