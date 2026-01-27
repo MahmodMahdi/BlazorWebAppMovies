4b79eff063f09d3b3f0a929854281130119549a9
MahmoudAmin <mahmoudElmahdy555@gmail.com>
2026-01-27 22:09:13 +0200

    feat: Implement Movie Dtos

---
+++ Dtos/Movie/MovieUpdateDto.cs
@@ -0,0 +1,10 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace BlazorWebAppMovies.Dtos.Movie
+{
+	public class MovieUpdateDto : MovieCreateDto
+	{
+		[Required]
+		public int Id { get; set; }
+	}
+}
