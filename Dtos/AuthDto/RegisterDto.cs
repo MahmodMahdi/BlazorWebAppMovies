e9ae0e9494c0841f2970a7bb66f57f1eb38467c5
MahmoudAmin <mahmoudElmahdy555@gmail.com>
2026-01-30 16:20:57 +0200

    feat: Add authentication dtos to transform data from base entity

---
+++ Dtos/AuthDto/RegisterDto.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace BlazorWebAppMovies.Dtos.AuthDto
+{
+	public class RegisterDto
+	{
+		[Required,MinLength(3),MaxLength(60)]
+		public string Name { get; set; }
+		[Required,EmailAddress]
+		public string Email { get; set; }
+		[Required]
+		public string Password { get; set; }
+		[Required]
+		[Compare(nameof(Password), ErrorMessage = "Passwords do not match")]
+		public string ConfirmPassword { get; set; }
+	}
+}
